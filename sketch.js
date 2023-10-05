236b558c152eb4fcb8395575ac7754d4a9102a81
avnisharma1369 <135978929+avnisharma1369@users.noreply.github.com>
2023-10-05 19:57:37 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var splashimg
+
+function preload(){
+splashimg=loadImage("splash.gif")
+
+}
+
+function setup(){
+    createCanvas(windowWidth,windowHeight)
+
+
+}
+
+function  draw(){
+background(splashimg)
+
+    
+}
